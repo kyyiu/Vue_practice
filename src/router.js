c2af42b0498ee1ef7139b568870ea95706f3a7e5
kyyiu <1308936944@qq.com>
2020-07-29 09:15:09 +0800

    n

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+import Home from './components/Home.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/home', component: Home }
+  ]
+})
+
+// 挂载路由导航守卫
+// 如果用户没有登录,但是直接通过url访问特定页面,需要重新导航到登录页面
+router.beforeEach((to, fromA, next) => {
+  // to 将要访问的路径
+  // fromA 代表从那个路径跳转而来
+  //  next 是一个函数表示放行
+  //  next() 放行 next('/login')强制跳转
+  if (to.path === '/login') return next()
+  //  获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+export default router

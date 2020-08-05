18ecf248e9dc8677a2062063d8029e68dea1b42b
kyyiu <1308936944@qq.com>
2020-08-05 14:38:44 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 需要安装运行依赖vue-table-with-tree-grid
 import TreeTable from 'vue-table-with-tree-grid'
+// 需要安装运行依赖vue-quill-editor
+// 导入富文本编辑器,及相关样式
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -23,6 +29,19 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器,注册为全局可用的组件
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  // padStart 不足两位前面补0
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 new Vue({
   router,
   render: h => h(App)
